0a268934fd3428bd65d771af0ca6e9217918476c
StarZ <5440909@qq.com>
2020-01-20 00:10:24 +0800

    1

--- src/config.h
+++ src/config.h
@@ -10,10 +10,10 @@ const uint32_t deviceId = ESP.getChipId();
 #define Esp01_Led_Count 24
 #define Esp01_Pin 2
 
-#define mqttServer            "home.dxhd.cc"    //"broker.hivemq.com"
+#define mqttServer            "broker.hivemq.com"
 #define mqttPort              1883
-#define mqttUsername          "admin"
-#define mqttPassword          "admin"
+#define mqttUsername          ""
+#define mqttPassword          ""
 #define mqttFeed              "home/StarZ_%s/" // %s will be replaced by the device id
 #define mqttQos               1
 #define mqttRetain            1
